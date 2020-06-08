a5a3c6e897f3d0a31c4b6a62aae7ae471d16188b
kimloong <312891416@qq.com>
2020-06-08 23:56:41 +0800

    增加1920*1200壁纸

---
+++ scripts/wallpaper/download_1920_1200.sh
@@ -0,0 +1,12 @@
+export ALL_PROXY=socks5://127.0.0.1:1080
+wget https://w.wallhaven.cc/full/13/wallhaven-13vym3.jpg -O ~/wallpaper/1.jpg
+wget https://w.wallhaven.cc/full/zm/wallhaven-zmr6qv.png -O ~/wallpaper/2.png
+wget https://w.wallhaven.cc/full/4l/wallhaven-4lr5wl.jpg -O ~/wallpaper/3.jpg
+wget https://w.wallhaven.cc/full/g8/wallhaven-g8e8yd.jpg -O ~/wallpaper/4.jpg
+wget https://w.wallhaven.cc/full/dg/wallhaven-dgzj9o.jpg -O ~/wallpaper/5.jpg
+wget https://w.wallhaven.cc/full/48/wallhaven-48wk6k.jpg -O ~/wallpaper/6.jpg
+
+# https://wallhaven.cc/w/mde631
+# https://wallhaven.cc/w/ymrxp7
+# https://wallhaven.cc/w/vgrqj8
+unset ALL_PROXY
